636a39981c70f89063cca578efadb2553afb2c58
stefany laforest <stefanylaforest@live.ca>
2021-05-27 22:34:07 +0100

    getSingleBrand + getAllProducts handlers

--- server/handlers.js
+++ server/handlers.js
@@ -1,13 +1,28 @@
 const brands = require("./data/companies.json");
 const items = require("./data/items.json");
 
-const getAllProducts = () => {};
+const getAllProducts = (req, res) => {
+  res.status(200).json({ status: 200, message: "success", data: items });
+};
 
 const getAllBrands = () => {};
 
 const getSingleProduct = () => {};
 
-const getSingleBrand = () => {};
+const getSingleBrand = (req, res) => {
+  const { brandName } = req.params;
+  const result = brands.filter((brand) => {
+    return brand.name.toLowerCase().includes(brandName.toLowerCase());
+  });
+  if (result.length > 0) {
+    res.json({ status: 200, message: "success", data: result });
+  } else {
+    res.json({
+      status: 404,
+      message: `sorry, we do not hold ${result} in our store 😔`,
+    });
+  }
+};
 
 const getAllCategories = () => {};
 
