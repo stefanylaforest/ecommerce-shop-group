370de97eef6e24348751720edd51190f2b5955e1
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-05-28 17:38:58 -0400

    collection unfinished

--- client/src/components/CollectionPage.js
+++ client/src/components/CollectionPage.js
@@ -1,7 +1,108 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Styled from "styled-components";
+
+import { AppContext } from "./AppContext";
+import ProductCard from "./ProductCard";
+
+const initialFilters = { brands: ["garmin"] , categories: ["Lifestyle"], bodyLocation: ['head']};
 
 const CollectionPage = () => {
-  return <div></div>;
+  const { products, setItemsInCart, brands } = useContext(AppContext);
+
+  const [productsFiltered, setProductsFiltered] = useState(products);
+
+  const [filters, setFilters] = useState(initialFilters);
+
+  const handleFilterCahnge = (event) => {
+    const filterType = event.target.name;
+    const value = event.target.value;
+    if (event.target.checked) {
+      setFilters({ ...filters, [filterType]: [...filters[filterType], value] });
+    } else {
+      const newFiltersOfThatType = filters[filterType].filter(
+        (i) => i !== value
+      );
+      const newFilters = { ...filters, [filterType]: newFiltersOfThatType };
+      setFilters(newFilters);
+    }
+  };
+  console.log("Filters are:", filters);
+
+  return (
+    <Div>
+      <div className="filter-box">
+        <h2>Filters:</h2>
+
+        <p>Categories</p>
+        <details>
+          <summary>Brands</summary>
+          {brands.map((brand) => (
+            <div className="checkbox">
+              <input
+                type="checkbox"
+                value={brand.name}
+                name="brands-filter"
+                onChange={handleFilterCahnge}
+              />
+              <lable>{brand.name}</lable>
+            </div>
+          ))}
+        </details>
+        <p>Body Location</p>
+      </div>
+      <div className="wrapper">
+        <div className="control-box">
+          <select>
+            <option>Price: Lowest to Highest</option>
+            <option>Price: Highest to Lowest</option>
+            <option>A to Z</option>
+            <option>Z to A</option>
+          </select>
+        </div>
+        <div className="collection">
+          {products.map((product) => (
+            <ProductCard product={product} />
+          ))}
+        </div>
+      </div>
+    </Div>
+  );
 };
 
 export default CollectionPage;
+
+const Div = Styled.div`
+
+background: lightblue;
+display: flex;
+
+.collection {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  
+}
+
+.control-box {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.filter-box {
+  background: lightgreen;
+  padding: 1rem;
+  flex: 30%;
+}
+
+.checkbox {
+  padding: 0.1rem;
+  border: 2px solid gray;
+  border-radius: 10px;
+}
+
+.wrapper {
+  flex: 70%;
+}
+
+
+`;
--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Styled from "styled-components";
 
-const ProductCard = () => {
-  return <div></div>;
+const ProductCard = ({ product }) => {
+  const {
+    name,
+    price,
+    body_location,
+    category,
+    _id,
+    imageSrc,
+    numInStock,
+    companyId,
+  } = product;
+  return (
+    <Div>
+      <img
+        className="product-image"
+        src={imageSrc}
+        alt="product image"
+        width="100%"
+        />
+        <h2>{name}</h2>
+      <p>for: {body_location}</p>
+      <p>Price: {price}</p>
+      <button>ADD TO CART</button>
+    </Div>
+  );
 };
 
 export default ProductCard;
+
+const Div = Styled.div`
+
+border: 1px solid gray;
+flex: 30%;
+
+.product-image {
+  background: yellow;
+}
+
+h2 {
+  font-size: 0.9em;
+}
+
+border-radius: 10px;
+
+`;
