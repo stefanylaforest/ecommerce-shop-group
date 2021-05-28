e787c9b87a82cefbfe6405012d30f4a4fb6688e1
stefany laforest <stefanylaforest@live.ca>
2021-05-28 17:43:39 +0100

    added some basic global styles

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Hompage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Watch Store</h1>
+      <h2>Watch Store</h2>
+      <h3>Watch Store</h3>
+      <p>Watch Store</p>
+      <p>WATCH STORE</p>
+      <button className="accentBtn">View Product</button>
+      <button className="accentBtn">VIEW PRODUCT</button>
+    </div>
+  );
 };
 
 export default Hompage;
