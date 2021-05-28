37ea4e43cde63e45d207c3513c67d3f918079392
stefany laforest <stefanylaforest@live.ca>
2021-05-28 17:47:04 +0100

    homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-const Hompage = () => {
-  return (
-    <div>
-      <h1>Watch Store</h1>
-      <h2>Watch Store</h2>
-      <h3>Watch Store</h3>
-      <p>Watch Store</p>
-      <p>WATCH STORE</p>
-      <button className="accentBtn">View Product</button>
-      <button className="accentBtn">VIEW PRODUCT</button>
-    </div>
-  );
+const Homepage = () => {
+  return <div></div>;
 };
 
-export default Hompage;
+export default Homepage;
