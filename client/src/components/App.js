857ba41d1d5efcdcf390ba7e6e543c0c5b865ee6
Hamid Keshmiri <78935540+hamidkd@users.noreply.github.com>
2021-05-28 11:45:18 -0400

    create app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   // const [bacon, setBacon] = useState(null);
@@ -9,7 +10,36 @@ function App() {
   //     .then(data => setBacon(data));
   // }, []);
 
-  return <div>Watch Store</div>;
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Cart />
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/products">
+            <Collection />
+          </Route>
+          <Route exact path="/products/:productId">
+            <ProductDetails />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+          <Route exact path="/confirmtion">
+            <Confirmation />
+          </Route>
+          <Route exact path="/view-order">
+            <ViewOrder />
+          </Route>
+        </Switch>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
