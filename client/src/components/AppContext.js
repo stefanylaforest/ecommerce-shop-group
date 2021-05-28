3118b7da2643b7f3603172a5fe0db2c7f1c40d7a
Junhwan Im <junhwanim@hotmail.com>
2021-05-28 11:55:33 -0400

    added value for context

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -3,24 +3,27 @@ import React, { createContext, useState, useEffect } from "react";
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-const  [products, setProducts] = useState([]);
-const  [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [brands, setBrands] = useState([]);
 
-useEffect(()=>{
-  fetch("/api/products")
-  .then((rest) => rest.json())
-  .then((json) => setProducts(json.data));
-},[])
+  useEffect(() => {
+    fetch("/api/products")
+      .then((rest) => rest.json())
+      .then((json) => setProducts(json.data));
+  }, []);
 
-useEffect(()=>{
-  fetch("/api/brands")
-  .then((rest) => rest.json())
-  .then((json) => setBrands(json.data));
-},[])
+  useEffect(() => {
+    fetch("/api/brands")
+      .then((rest) => rest.json())
+      .then((json) => setBrands(json.data));
+  }, []);
 
-// console.log(products)
-// console.log(brands)
+  // console.log(products)
+  // console.log(brands)
 
-  return (<AppContext.Provider>{children}</AppContext.Provider>);
+  return (
+    <AppContext.Provider value={{ products, brands }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
-
