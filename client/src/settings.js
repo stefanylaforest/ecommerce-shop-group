2a02d33883da8e828934b84a0a9c753d99ab3601
Junhwan Im <junhwanim@hotmail.com>
2021-05-27 23:09:58 -0400

    made two endpoints and settings.js for frontend

---
+++ client/src/settings.js
@@ -0,0 +1,23 @@
+// required informations on purchase.
+export const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  address: "",
+  city: "",
+  province: "",
+  country: "",
+  creditCardNum: "",
+  expirationDate: "",
+  itemId: "",
+  quantity: "",
+};
+
+// if error happens, those statements specify where and why error happens.
+export const errorMessages = {
+    "missing-entry": "Please fill out all entries.",
+    "missing-@": "Email includes @.",
+    "missing-card-number": "Credit-Card number consists of 16 digits",
+    "missing-/": "Expiration-Date inlcudes / after first two digits.",
+    "invalidation": "Expiration-Date consists of 4 digits of number and /. ex) 07/22"
+};
--- server/handlers.js
+++ server/handlers.js
@@ -1,12 +1,15 @@
 const brands = require("./data/companies.json");
 const items = require("./data/items.json");
 const orders = require("./data/orders.json");
+const { v4: uuidv4 } = require("uuid");
 
 const getAllProducts = (req, res) => {
   res.status(200).json({ status: 200, message: "success", data: items });
 };
 
-const getAllBrands = () => {};
+const getAllBrands = (req, res) => {
+  res.status(200).json({ status: 200, messege: "It worked", data: brands });
+};
 
 const getSingleProduct = (req, res) => {
   const { productId } = req.params;
@@ -51,7 +54,69 @@ const getAllCategories = (req, res) => {
     .json({ status: 200, message: "success", data: uniqueCategories });
 };
 
-const createOrder = () => {};
+const createOrder = (req, res) => {
+  // required informations
+  const {
+    firstName,
+    lastName,
+    email,
+    address,
+    city,
+    province,
+    country,
+    creditCardNum,
+    expirationDate,
+    itemId,
+    quantity,
+  } = req.body;
+
+  let status = "";
+  let error = "";
+
+  // primarily checking if all required informations are filled out.
+  if (
+    firstName &&
+    lastName &&
+    email &&
+    address &&
+    city &&
+    province &&
+    country &&
+    creditCardNum &&
+    expirationDate &&
+    itemId &&
+    quantity
+  ) {
+    status = "success"
+    // validation for email.
+    if (!email.split("").includes("@")) {
+      return res.json({ status: "error", error: "missing-@" });
+    }
+    // validation for the number of credit card.
+    if (creditCardNum.length !== 16) {
+      return res.json({ status: "error", error: "missing-card-number" });
+    }
+    // validation for the number of expiration date.
+    if (expirationDate.length !== 5) {
+      return res.json({ status: "error", error: "invalidation" });
+    }
+    // validation for the special symbol of expiration date.
+    if (!expirationDate.split("").includes("/")) {
+      return res.json({ status: "error", error: "missing-/" });
+    } else {
+      // setting status accordingly, outputing data, generating random order-number.
+      return res.json({
+        status: status,
+        error: error,
+        data: { ...req.body, orderNum: uuidv4() },
+      });
+    }
+  } else {
+    return res.json({ status: "error", error: "missing-entry" });
+  }
+};
+
+
 
 const getOrderById = (req, res) => {
   const { orderId } = req.params;
